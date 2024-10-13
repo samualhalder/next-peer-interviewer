0b5a5a9ffbfba850cbbdd06eb226a2f0a4d46d7c
samualhalder <samual.halder2020@gmail.com>
2024-10-14 00:46:59 +0530

    sign up functionality

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,40 @@
+import prisma from "@/lib/db";
+import bcrypt from "bcrypt";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    console.log("sign up hit");
+
+    if (!body.username || !body.email || !body.password) {
+      return new Response("please fill all the fields,", { status: 400 });
+    }
+    if (body.password.length < 6) {
+      return new Response("password must be more than 5 charecters.", {
+        status: 400,
+      });
+    }
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
+    if (findUser) {
+      return new Response("user already exists.", { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    await prisma.user.create({
+      data: {
+        username: body.username,
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+    return new Response("user saved successfully", { status: 200 });
+  } catch (err) {
+    return new Response(`internal server error ${err}`, {
+      status: 500,
+    });
+  }
+}
--- src/app/components/AuthCard.tsx
+++ src/app/components/AuthCard.tsx
@@ -1,32 +1,100 @@
-import React from "react";
-import { Button, Divider, TextField } from "@mui/material";
+"use client";
+import React, { useState } from "react";
+import { Alert, Button, Divider, TextField } from "@mui/material";
 import { FcGoogle } from "react-icons/fc";
 
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function AuthCard({ page }: { page: string }) {
   const redirectPage = page === "Sign In" ? "signup" : "signin";
+  const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [fromData, setFromData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFromData({ ...fromData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setErrorMessage(null);
+    if (redirectPage !== "signup") {
+      try {
+        const response = await fetch("/api/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(fromData),
+        });
+        const data = await response.text();
+        if (!response.ok) {
+          setErrorMessage(data);
+        } else {
+          router.push("/signin");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("for sign in");
+    }
+  };
 
   return (
-    <div className=" shadow-2xl w-[400px] h-[500px] p-10 flex justify-center items-center flex-col relative gap-3 rounded-md">
+    <div className=" shadow-2xl w-[400px] min-h-[600px] p-10 flex justify-center items-center flex-col relative gap-3 rounded-md">
       <h1 className=" absolute top-3 text-3xl">{page} </h1>
-      <form action="" className="w-full flex  flex-col gap-5">
+      <form
+        action=""
+        className="w-full flex  flex-col gap-5"
+        onSubmit={handleSubmit}
+      >
         <div>
           <TextField
             className="w-[95%]"
             label="Email"
             type="Email"
+            name="email"
+            value={fromData.email}
+            onChange={(e) => handleChange(e)}
             autoComplete="current-email"
           />
         </div>
+        {redirectPage !== "signup" && (
+          <div>
+            <TextField
+              className="w-[95%]"
+              label="Username"
+              type="text"
+              name="username"
+              value={fromData.username}
+              onChange={(e) => handleChange(e)}
+              autoComplete="current-email"
+            />
+          </div>
+        )}
         <div>
           <TextField
             className="w-[95%]"
             label="Password"
             type="password"
+            name="password"
+            value={fromData.password}
+            onChange={(e) => handleChange(e)}
             autoComplete="current-password"
           />
         </div>
-        <Button className="w-[95%]" variant="contained">
+        {errorMessage && (
+          <Alert className="w-[95%]" severity="error">
+            {errorMessage}
+          </Alert>
+        )}
+        <Button className="w-[95%]" variant="contained" type="submit">
           {page}
         </Button>
       </form>
@@ -41,7 +109,9 @@ export default function AuthCard({ page }: { page: string }) {
         className="text-blue-500 mt-10 hover:text-blue-300"
         href={`/${redirectPage}`}
       >
-        already have an account ? {redirectPage}
+        {redirectPage !== "signin"
+          ? "don't have an account? sign up"
+          : "already have an account? sign in"}
       </Link>
     </div>
   );
